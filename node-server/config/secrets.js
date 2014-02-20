383f3745aa19eff2d9fd1911f1930f32be3abdf5
Adam Auckland <adamauckland@gmail.com>
2014-02-20 15:47:56 +0000

    Upload decorator

--- node-server/config/secrets.js
+++ node-server/config/secrets.js
@@ -1,7 +1,7 @@
 module.exports = {
-	db: '192.168.1.110',
+	db: 'Boxa',
 	// db: '127.0.0.1',
-	dbconnection: 'mongodb://192.168.1.110:27017/agilitynet',
+	dbconnection: 'mongodb://Boxa:27017/agilitynet',
 	// dbconnection: 'mongodb://127.0.0.1:27017/agilitynet',
 
 	sendgrid: {
--- node-server/controllers/upload.js
+++ node-server/controllers/upload.js
@@ -6,55 +6,126 @@ var fsExtra = require('fs.extra');
 var util = require('util');
 var path = require('path');
 var uuid = require('node-uuid');
-
+var gm = require('gm');
+var imageMagick = gm.subClass({ imageMagick: true });
 var User = require('../models/mongoose/user');
 
-exports.uploadFile = function (request, response) {
-	var instream;
-	var outstream;
-	var outPath;
 
+
+
+exports.uploadFile = UploadManager({
+	scale: [
+		{
+			width: 200,
+			height: 100
+		}
+	]
+}, function (request, response, data) {
 	User.findById(request.user.id, function (err, user) {
-		var fileItem = request.files;
-		var tempPath = fileItem.file.path;
-		var originalFilename = fileItem.file.originalFilename;
-		var originalExtension = originalFilename.split('.');
-		originalExtension = originalExtension[originalExtension.length - 1];
+		user.profile.picture = data.assetPath + '/' + _.findWhere(data.scaled, { width: 200 }).url;
 
-		var userDirectory = path.resolve(__dirname + '/../../workspace/photos/' + user._id + '/');
+		user.save(function (err, user) {
+			response.send(200);
+		});
+	});
+});
+
+
+
+
+function UploadManager(options, fn) {
+	function UploadModelHandlerPrototype(request, response) {
+
+		var data = {};
+		var instream;
+		var outstream;
+		var outPath;
 		var today = new Date();
+		var fileItem = request.files;
 
-		var stampedDirectory = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + '/';
-		var newDirectory = userDirectory + '/' + stampedDirectory;
+		data.uuid = uuid.v4();
+		data.tempPath = fileItem.file.path;
+		data.originalFilename = fileItem.file.originalFilename;
+		data.originalExtension = data.originalFilename.split('.');
+		data.originalExtension = '.' + data.originalExtension[data.originalExtension.length - 1];
 
-		console.log(newDirectory);
+		data.photosLocalDirectory = path.resolve(__dirname + '/../../workspace/photos/');
 
-		fsExtra.mkdirRecursive(newDirectory, function() {
-			var newFileName = stampedDirectory + uuid.v4() + '.' + originalExtension;
-			var outPath = userDirectory + '/' + newFileName;
+		data.stampedDirectory = request.user.id + '/' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + '/' + data.uuid;
 
-			console.log(tempPath);
-			console.log(outPath);
+		data.newLocalDirectory = data.photosLocalDirectory + '/' + data.stampedDirectory;
 
-			instream = fs.createReadStream(tempPath);
-			outstream = fs.createWriteStream(outPath);
+		console.log(data.newDirectory);
+
+		fsExtra.mkdirRecursive(data.newLocalDirectory, function () {
+			data.outPath = data.newLocalDirectory + '/' + 'source' + data.originalExtension;
+			data.assetPath = '/workspace/photos/';
+			data.newUrlPath = data.assetPath + data.stampedDirectory + '/source' + data.originalExtension;
+
+			instream = fs.createReadStream(data.tempPath);
+			outstream = fs.createWriteStream(data.outPath);
 
 			util.pump(instream, outstream, function (err) {
 				if (err) {
 					console.log(err);
 				} else {
 
-					user.profile.picture = '/workspace/photos/' + user._id + '/' + newFileName;
+					console.log('now resize');
+					// now the image is saved
+					//
+					var scaleIndex = 0;
+					data.scaled = [];
 
-					user.save(function (err, user) {
-						response.send(200);
-					});
+					function finishedScaling () {
+						fn(request, response, data);
+					}
+
+					function scaleNextItem () {
+						if (scaleIndex < options.scale.length) {
+							var width = options.scale[scaleIndex].width;
+							var height = options.scale[scaleIndex].height;
+							var newScaledObject = {
+								width: width,
+								height: height,
+								name: 'source_' + width + 'x' + height + data.originalExtension
+							};
+
+							newScaledObject.localPath = data.newLocalDirectory + '/' + newScaledObject.name;
+							newScaledObject.url = data.stampedDirectory + '/' + newScaledObject.name;
+
+							data.scaled.push(newScaledObject);
+
+							console.log('From: ' + data.outPath);
+							console.log('To:  ' + newScaledObject.localPath);
+
+							imageMagick(data.outPath)
+								.resize(width, height)
+								.autoOrient()
+								.write(newScaledObject.localPath, function (err) {
+									if (err) {
+										console.log(err);
+									}
+									if (!err) {
+										console.log(' hooray! ');
+										scaleIndex++;
+										scaleNextItem();
+									}
+								});
+						} else {
+							finishedScaling();
+						}
+					}
+
+					scaleNextItem();
 				}
 			});
 		});
+	}
+
+	return UploadModelHandlerPrototype;
+}
 
 
-	});
-};
+
 
 exports.uploadPath = uploadPath;
