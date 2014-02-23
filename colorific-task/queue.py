36b07e01acf3099be0b32a8fca65ed5b3b230686
Adam Auckland <adamauckland@gmail.com>
2014-02-23 11:50:04 +0000

    adding settings

--- colorific-task/queue.py
+++ colorific-task/queue.py
@@ -6,49 +6,63 @@ import sys
 import optparse
 import json
 import shutil
-
+import pika
 import colorific
 from colorific.palette import (
     extract_colors, print_colors, save_palette_as_image, color_stream_mt,
     color_stream_st)
-from celery import Celery
+import settings
 
-def create_directory(directory_name):
-    if not os.path.exist(directory_name):
-        os.makedirs(directory_name)
 
-INPUT_FILE_QUEUE = os.path.abspath('./queue/in')
-OUTPUT_FILE_QUEUE = os.path.abspath('./queue/out/')
-PROCESSED_FILE_QUEUE = os.path.abspath('./queue/processed/')
+def connect_to_queue():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
 
-create_directory(INPUT_FILE_QUEUE)
-create_directory(OUTPUT_FILE_QUEUE)
-create_directory(PROCESSED_FILE_QUEUE)
+    return connection, channel
 
-input_files = os.listdir(INPUT_FILE_QUEUE)
 
-for input_file in input_files:
-    filename = os.path.join(INPUT_FILE_QUEUE, input_file)
+def read_file_queue(settings, input_files):
+    for input_file in input_files:
+        filename = os.path.join(settings.INPUT_FILE_QUEUE, input_file)
 
-    palette = extract_colors(
-        filename,
-        min_saturation=0.05,
-        min_prominence=0.01,
-        min_distance=10.0,
-        max_colors=5,
-        n_quantized=100)
+        palette = extract_colors(
+            filename,
+            min_saturation=0.05,
+            min_prominence=0.01,
+            min_distance=10.0,
+            max_colors=5,
+            n_quantized=100)
 
-    colour_data = print_colors(filename, palette)
-    parsed_data = colour_data.split()[1].split(',')
+        colour_data = print_colors(filename, palette)
+        parsed_data = colour_data.split()[1].split(',')
 
-    data = {
-        'filename': input_file,
-        'data': parsed_data
-    }
+        data = {
+            'filename': input_file,
+            'data': parsed_data
+        }
 
-    out_file = os.path.join(OUTPUT_FILE_QUEUE, input_file + '_data.json')
-    with open(out_file, 'wt') as out_file:
-        out_file.write(json.dumps(data))
+        out_file = os.path.join(settings.OUTPUT_FILE_QUEUE, input_file + '_data.json')
+        with open(out_file, 'wt') as out_file:
+            out_file.write(json.dumps(data))
 
-    shutil.move(filename, os.path.join(PROCESSED_FILE_QUEUE, input_file))
+        shutil.move(filename, os.path.join(settings.PROCESSED_FILE_QUEUE, input_file))
 
+
+def queue_watcher(settings):
+    connection, channel = connect_to_queue()
+    channel.queue_declare(queue='colorific')
+
+    def callback(ch, method, properties, body):
+        print " [x] Received %r" % (body,)
+
+    channel.basic_consume(
+        callback,
+        queue='colorific',
+        no_ack=True)
+
+    print ' [*] Waiting for messages. To exit press CTRL+C'
+    channel.start_consuming()
+
+
+if __name__=="__main__":
+    queue_watcher(settings)
---
+++ colorific-task/settings.py
@@ -0,0 +1,19 @@
+import os
+import os.path
+
+
+WORKSPACE = os.path.abspath("../workspace/colorific")
+INPUT_FILE_QUEUE = os.path.join(WORKSPACE, "queue/in")
+OUTPUT_FILE_QUEUE = os.path.join(WORKSPACE, "queue/out/")
+PROCESSED_FILE_QUEUE = os.path.join(WORKSPACE, "queue/processed/")
+
+
+def create_directory(directory_name):
+    if not os.path.exist(directory_name):
+        os.makedirs(directory_name)
+
+
+create_directory(WORKSPACE)
+create_directory(INPUT_FILE_QUEUE)
+create_directory(OUTPUT_FILE_QUEUE)
+create_directory(PROCESSED_FILE_QUEUE)
