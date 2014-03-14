a6d267c95b22e9c5f612a71b0c123a080312ced8
Kristina Auckland <kristina.auckland@gmail.com>
2014-03-14 10:23:32 +0000

    Whitespace

--- angular-client/app/scripts/controllers/feedback.js
+++ angular-client/app/scripts/controllers/feedback.js
@@ -4,7 +4,5 @@ app.controller('FeedbackController', [
 	'$scope', 'Mapdata', 'ShowService', 'ProfileService', 'RelationshipService', '$location', '$route', '$routeParams',
 	function FeedbackController($scope, Mapdata, ShowService, ProfileService, RelationshipService, $location, $route, $routeParams) {
 		"use strict";
-
-
 	}
 ]);
